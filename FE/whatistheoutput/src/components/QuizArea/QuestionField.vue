659e31b6fe3d244df6d5db459b02d88627f244a2
istvanamolnar <istvan0988@gmail.com>
2019-09-16 22:53:37 +0200

    feat(background-pattern)

--- FE/whatistheoutput/src/components/QuizArea/QuestionField.vue
+++ FE/whatistheoutput/src/components/QuizArea/QuestionField.vue
@@ -14,14 +14,16 @@ export default {
 </script>
 
 <style scoped>
-@media all and (max-height: 420px) {
-  .question-text {
-    font-size: 2.5vw;
+  @media all and (max-height: 420px) {
+    .question-text {
+      font-size: 2.5vw;
+    }
   }
-}
+  
   .question-field {
-    background-color: rgba(255, 255, 255, 0.5);
+    background-color: rgba(255, 255, 255, 0.8);
     font-size: 24px;
     font-size: 2.5vh;
   }
+
 </style>
