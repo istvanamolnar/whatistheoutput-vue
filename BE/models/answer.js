d044b18ea11419b0fa82ac60f3407cfefdca6556
istvanamolnar <istvan0988@gmail.com>
2019-09-24 17:18:57 +0200

    refactor(file system)

--- BE/models/answer.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const answerSchema = mongoose.Schema({
-  answer: { type: String, required: true},
-  isCorrect: { type: Number, required: true}
-});
-
-module.exports = mongoose.model('Answer', answerSchema);
